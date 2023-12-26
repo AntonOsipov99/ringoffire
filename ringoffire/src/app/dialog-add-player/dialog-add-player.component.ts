714205e9344643b7abaef7fc3c1b35c8f2ba3a3c
AntonOsipov99 <antonosipov@gmx.de>
2023-12-26 17:06:40 +0100

    update ring of fire

---
+++ ringoffire/src/app/dialog-add-player/dialog-add-player.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-dialog-add-player',
+  standalone: true,
+  imports: [MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
+  templateUrl: './dialog-add-player.component.html',
+  styleUrl: './dialog-add-player.component.scss'
+})
+export class DialogAddPlayerComponent {
+  name: string = '';
+
+}
